002e8f8f24c2ddf8fd53e1e7bedd640ac486542c
Tom-Hazell-EPQ <Tom-Hazell-EPQ@users.noreply.github.com>
2014-09-16 19:15:23 +0100

    Actualy Synced Code With Laptop/ Added Comments

--- src/com/nutty/EPQ/app/PrefsFragment.java
+++ src/com/nutty/EPQ/app/PrefsFragment.java
@@ -3,14 +3,18 @@ package com.nutty.EPQ.app;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
+/*
+ * This is required to show the prefrences as there own activity
+ * @author Tom Hazell (C) 2014 Tom Hazell
+ */
 public class PrefsFragment extends PreferenceFragment {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		// Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.prefrences);
+
+		// Load the preferences from the XML file
+		addPreferencesFromResource(R.xml.prefrences);
 	}
 
 }
--- src/com/nutty/EPQ/app/ShowPreference.java
+++ src/com/nutty/EPQ/app/ShowPreference.java
@@ -2,14 +2,17 @@ package com.nutty.EPQ.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+/*
+ * this uses the preference fragment class to xml preferences
+ * 
+ */
 public class ShowPreference extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
+		//sets content to PrestFragment class
 		getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new PrefsFragment()).commit();
 	}
